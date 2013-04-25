13da0eef4d8ae6f1c81826ec8313db68876ec1ad
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2013-04-24 22:33:50 -0400

    Handle sign in attempt for email without a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.where(email: params[:email]).first
-    render status: 403 and return unless @user.authenticate(params[:password])
+
+    unless @user.present? && @user.authenticate(params[:password])
+      render action: :new, status: 403 and return
+    end
+
     session[:user_id] = @user.id
     redirect_to root_path
   end
